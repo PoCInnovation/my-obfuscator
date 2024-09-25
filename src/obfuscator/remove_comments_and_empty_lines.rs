5b98b4bb2a5f037ee7f489aa9ff3a86bf6feacc2
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2024-09-25 21:39:45 +0200

    added comment removal too

--- src/obfuscator/remove_comments_and_empty_lines.rs
+++ src/obfuscator/remove_comments_and_empty_lines.rs
@@ -1,13 +1,45 @@
 use super::error::ObfuscatorError;
 use super::Obfuscator;
 use super::Result;
+use super::Shiftable;
+use tree_sitter::{Tree, TreeCursor};
+
+fn get_comments(tree: &Tree) -> Vec<std::ops::Range<usize>> {
+    fn go(cursor: &mut TreeCursor, comments: &mut Vec<std::ops::Range<usize>>) {
+        let node = cursor.node();
+        if node.kind() == "comment" {
+            comments.push(node.start_byte()..node.end_byte());
+        }
+        if cursor.goto_first_child() {
+            go(cursor, comments);
+            cursor.goto_parent();
+        }
+        while cursor.goto_next_sibling() {
+            go(cursor, comments);
+        }
+    }
+    let mut comments = Vec::new();
+
+    go(&mut tree.walk(), &mut comments);
+
+    comments
+}
 
 impl Obfuscator {
     pub fn remove_comments_and_empty_lines(&mut self) -> Result<()> {
+        let comments = get_comments(&self.tree);
+
+        let mut shift = 0;
+        comments.into_iter().for_each(|comment| {
+            let len = comment.len();
+
+            self.code.replace_range(comment.shift(shift), "");
+            shift -= len as i32;
+        });
         self.code = self
             .code
             .lines()
-            .filter(|line| {!line.trim().is_empty()})
+            .filter(|line| !line.trim().is_empty())
             .collect::<Vec<_>>()
             .join("\n");
         self.reparse(ObfuscatorError::RemoveCommentsAndEmptyLines)
